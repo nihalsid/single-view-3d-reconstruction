Fixed projection (pointcloud and ifnet were not living in the same space) + batching Allow for 16bit precision training (via argument) and appropriate changes to MC & ifnet Added Arguments for profiler/precision Unflipped depthmap prediction (this is being done by pointcloud projection)

data_processing/distance_to_depth.py
@@ -54,10 +54,11 @@ def coords_multiplication(A, B):
 def depth_to_camera(depth_map, f, cx, cy):
     #if depth_map.size() ## TO-DO: enable batching and non-batching mode. Currently only supports batched inputs
     bs = depth_map.shape[0]
-    u, v = torch.meshgrid(torch.arange(depth_map.shape[-2], device=depth_map.device), torch.arange(depth_map.shape[-1], device=depth_map.device))
+    v, u = torch.meshgrid(torch.arange(depth_map.shape[-2], device=depth_map.device), torch.arange(depth_map.shape[-1], device=depth_map.device))
     X = ((torch.multiply(u, depth_map) - cx * depth_map) / f)
     Y = -((torch.multiply(v, depth_map) - cy * depth_map) / f)
     Z = depth_map
+    #return X.flatten(), Y.flatten(), Z.flatten()
     return X.reshape((bs, -1)), Y.reshape((bs, -1)), Z.reshape((bs, -1))
 
 
@@ -113,6 +114,7 @@ def depth_to_gridspace(distance_map, intrinsic_path=None, read=True):
 
     # depth from camera to grid space
     depth_in_gridspace = (camera2frustum @ np.stack([X, Y, Z, np.ones_like(X)]))[:3, :].T
+    depth_in_gridspace = depth_in_gridspace.view(distance_map.shape[0], -1)
     return depth_in_gridspace
 
 def depthmap_to_gridspace(depthmap):
@@ -131,7 +133,8 @@ def depthmap_to_gridspace(depthmap):
     # depth from camera to grid space
     camera2frustum = camera2frustum.expand((X.shape[0],-1, -1))
     coords = torch.stack([X, Y, Z, torch.ones_like(X)]).transpose(1,0)
-    depth_in_gridspace = (camera2frustum @ coords)[:, :3].transpose(-1,-2)
+    #print(camera2frustum.shape, coords.shape)
+    depth_in_gridspace = (camera2frustum @ coords)[:,:3, :].transpose(-1,-2)
     return depth_in_gridspace
 
 def get_intrinsic(intrinsic_path):

dataset/scene_net_data.py
@@ -49,7 +49,7 @@ class scene_net_data(Dataset):
         df_foler = Path(self.dataset_path) / "processed" / self.splitsdir / item
         
         image = Image.open(sample_folder / "rgb.png")
-        image = image.transpose(Image.FLIP_LEFT_RIGHT)
+        #image = image.transpose(Image.FLIP_LEFT_RIGHT)
         rgb_img = self.input_transform(image)
 
         sample_target = torch.from_numpy(read_df(str(df_foler / "target.df"))).float().unsqueeze(0)
@@ -80,7 +80,7 @@ class scene_net_data(Dataset):
         focal_length = float(intrinsic_line_0[2:].split(',')[0])
         transform = FromDistanceToDepth(focal_length)
         depth_map = transform(distance_map).numpy().astype('float32', casting='same_kind')
-        depth_map = np.flip(depth_map, 1)
+        #depth_map = np.flip(depth_map, 1)
         depth_flipped = depth_map.copy()
         depthmap_target = self.target_transform(depth_flipped)    
         

model/ifnet.py
@@ -157,16 +157,21 @@ class IFNetFeatureExtractor128(nn.Module):
         p[:, :, 0], p[:, :, 1], p[:, :, 2] = [2 * points[:, :, 2], 2 * points[:, :, 1], 2 * points[:, :, 0]]
         p = p.unsqueeze(1).unsqueeze(1)
         p = torch.cat([p + d for d in self.displacments.to(p.device)], dim=2)  # (B,1,7,num_samples,3)
-        feature_0 = F.grid_sample(x, p)  # out : (B,C (of x), 1,1,sample_num)
 
+        p = p.to(x.dtype) #16bit support
+        feature_0 = F.grid_sample(x, p)  # out : (B,C (of x), 1,1,sample_num)
+        
         net = self.actvn(self.conv_in(x))
         net = self.conv_in_bn(net)
+        p = p.to(net.dtype) #16bit support
+
         feature_1 = F.grid_sample(net, p)  # out : (B,C (of x), 1,1,sample_num)
         net = self.maxpool(net)
 
         net = self.actvn(self.conv_0(net))
         net = self.actvn(self.conv_0_1(net))
         net = self.conv0_1_bn(net)
+
         feature_2 = F.grid_sample(net, p)  # out : (B,C (of x), 1,1,sample_num)
         net = self.maxpool(net)
 

trainer/trainer_scene_net.py
@@ -78,7 +78,7 @@ class SceneNetTrainer(pl.LightningModule):
             point_cloud = depthmap_to_gridspace(depthmap).squeeze().reshape(-1,3)
             
             #visualize outputs of network stages (depthmap, pointcloud, mesh)
-            visualize_depthmap(depthmap, output_vis_path / f"{base_name}_depthmap")
+            visualize_depthmap(depthmap, output_vis_path / f"{base_name}_depthmap", flip = True)
             visualize_point_list(point_cloud, output_vis_path / f"{base_name}_pc.obj")
             implicit_to_mesh(self.ifnet, batch['input'], np.round(self.dims).astype(np.int32), 0.5, output_vis_path / f"{base_name}_predicted.obj")
             #visualize_sdf(batch['target'].squeeze().cpu().numpy(), output_vis_path / f"{base_name}_gt.obj", level=1)
@@ -91,7 +91,8 @@ def train_scene_net(args):
     tb_logger = pl_loggers.TensorBoardLogger(save_dir=os.path.join("runs", 'logs/'))
     model = SceneNetTrainer(args)
     trainer = Trainer(gpus=args.gpu , num_sanity_val_steps=args.sanity_steps, checkpoint_callback=checkpoint_callback, max_epochs=args.max_epoch, limit_val_batches=args.val_check_percent,
-                      val_check_interval=min(args.val_check_interval, 1.0), check_val_every_n_epoch=max(1, args.val_check_interval), resume_from_checkpoint=args.resume, logger=tb_logger, benchmark=True)
+                      val_check_interval=min(args.val_check_interval, 1.0), check_val_every_n_epoch=max(1, args.val_check_interval), 
+                      resume_from_checkpoint=args.resume, logger=tb_logger, benchmark=True, profiler=args.profiler, precision=args.precision)
 
     trainer.fit(model)
 

util/visualize.py
@@ -21,7 +21,7 @@ def visualize_point_list(grid, output_path):
 
 
 def visualize_sdf(sdf, output_path, level=0.75):
-    vertices, triangles = mc.marching_cubes(sdf, level)
+    vertices, triangles = mc.marching_cubes(sdf.astype(float), level)
     mc.export_obj(vertices, triangles, output_path)
 
 
@@ -31,7 +31,7 @@ def visualize_grid(grid, output_path):
         base_mesh = trimesh.voxel.ops.multibox(centers=point_list, pitch=1)
         base_mesh.export(output_path)
         
-def visualize_depthmap(depthmap, output_path):
+def visualize_depthmap(depthmap, output_path, flip=False):
     if isinstance(depthmap, np.ndarray):
         depthmap = depthmap.squeeze()
 
@@ -40,7 +40,8 @@ def visualize_depthmap(depthmap, output_path):
 
     else:
         raise NotImplementedError
-
+    if flip:
+        depthmap = np.flip(depthmap, axis=1)
     rescaled = (255.0 / depthmap.max() * (depthmap - depthmap.min())).astype(np.uint8)
     im = Image.fromarray(rescaled)
     im.save(str(output_path) +'.png')